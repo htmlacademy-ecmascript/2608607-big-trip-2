66981838f4b5dd8897b06d1a1d29dd7afcf6017e
Yuri-tm <yuritm.tm@gmail.com>
2025-09-15 22:37:02 +0300

    refactor itinerary presenter: reorganize imports and update constructor parameters

--- src/presenter/itinerary-presenter.js
+++ src/presenter/itinerary-presenter.js
@@ -1,29 +1,32 @@
 import BoardView from '../view/board-view.js';
-import EventsListView from '../view/trip-events-list-view.js';
 import NewPointView from '../view/add-new-point-view.js';
+import EventEditView from '../view/event-edit-form-view.js';
+import EventItemView from '../view/events-item-view.js';
 import FiltersView from '../view/filters-view.js';
 import SortingView from '../view/sorting-view.js';
+import EventsListView from '../view/trip-events-list-view.js';
 import TripInfoView from '../view/trip-info-view.js';
-// import { TYPES, siteMainElement, tripEventsElement, tripEventsFilters, tripInfoElement } from '../const.js';
+
 import { render, RenderPosition } from '../render.js';
 
 export default class ItineraryPresenter {
   boardComponent = new BoardView();
-  eventListComponent = new EventsListView();
+  eventsListComponent = new EventsListView();
   newPointComponent = new NewPointView();
+  tripInfoComponent = new TripInfoView();
 
-  constructor({ boardContainer, filtersContainer }) {
+  constructor({ boardContainer, infoContainer }) {
     this.boardContainer = boardContainer;
-    this.tripEventsFilters = filtersContainer;
+    this.infoContainer = infoContainer;
+
   }
 
   init() {
-    render(new FiltersView(), this.tripEventsFilters, RenderPosition.BEFOREEND);
-    render(new FiltersView(), this.tripEventsFilters, RenderPosition.BEFOREEND);
-    render(new SortingView(), this.tripEventsElement, RenderPosition.BEFOREEND);
-    render(new TripInfoView(), this.tripInfoElement, RenderPosition.AFTERBEGIN);
-    render(this.eventListComponent, this.boardComponent.getElement());
-    // Render newPointComponent inside the event list
+    render(this.tripInfoComponent, this.boardContainer);
+    render(this.boardComponent, this.boardContainer);
+    render(new SortingView(), this.boardComponent.getElement(), RenderPosition.BEFOREEND);
+    render(this.eventsListComponent, this.boardComponent.getElement());
     render(this.newPointComponent, this.boardComponent.getElement());
+ 
   }
 }
